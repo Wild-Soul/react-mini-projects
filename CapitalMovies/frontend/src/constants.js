60504b8ab4096f0560f7ad3deae007ce560eaf1a
Rohit Kumar <30602486+Wild-Soul@users.noreply.github.com>
2021-11-18 22:45:22 +0530

    removed incomplete projects

--- CapitalMovies/frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const API_ENDPOINT = "https://api.themoviedb.org/3";
-export const DISCOVER = "DISCOVER";
-export const LATEST = "LATEST";
-export const FAVOURITES = "FAVOURITES";
-export const TRENDING = "TRENDING";
